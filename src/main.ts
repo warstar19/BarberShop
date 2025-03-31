43a3399f242329acaec680f8a50af82abd72071b
Carlos Obando <136123266+Z0MB13KV21@users.noreply.github.com>
2025-03-31 10:30:40 -0600

    funcion

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch(err => console.error(err));
+ import { provideHttpClient } from '@angular/common/http';
+ import { AppComponent } from './app/app.component';
+ import { appConfig } from './app/app.config';
+ import 'intersection-observer';
+ 
+ bootstrapApplication(AppComponent, appConfig)
+   .catch(err => console.error(err));
+ bootstrapApplication(AppComponent,
+   {
+     providers: [
+       provideHttpClient(), 
+       ...appConfig.providers     
+     ],
+     
+   }).catch(err => console.error(err));
